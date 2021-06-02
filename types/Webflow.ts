bef780692fc2f9e19c75591c0a97cb9442be5d16
Alex Iglesias <alexiglesias@finsweet.com>
2021-06-02 18:30:42 +0200

    Refactored the Webflow object.
    Now it can be undefined.
    The global declaration is moved to Webflow.ts
    The return of the require method can be undefined.

--- types/Webflow.ts
+++ types/Webflow.ts
@@ -5,8 +5,17 @@ interface Webflow {
   push: (callback: () => unknown) => void;
   destroy: () => void;
   ready: () => void;
-  require: (key: 'ix2') => { destroy: () => void; init: () => void };
+  require: (key: 'ix2') => { destroy: () => void; init: () => void } | undefined;
   env: () => boolean;
 }
 
 export default Webflow;
+
+/**
+ * Declare it globally
+ */
+declare global {
+  interface Window {
+    Webflow?: Webflow;
+  }
+}
--- webflow/restartWebflow.ts
+++ webflow/restartWebflow.ts
@@ -1,15 +1,9 @@
-import Webflow from '../types/Webflow';
-
-declare global {
-  interface Window {
-    Webflow: Webflow;
-  }
-}
-
 /**
  * Restart Webflow JS library
  */
 const restartWebflow = (): void => {
+  if (!window.Webflow) return;
+
   window.Webflow.destroy();
   window.Webflow.ready();
   window.Webflow.require('ix2')?.init();
