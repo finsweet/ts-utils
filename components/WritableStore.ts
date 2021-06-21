918816e3c30ed41d6658f7f826e9828fd359b1f6
Alex Iglesias <alexiglesias@finsweet.com>
2021-06-21 10:54:11 +0200

    Switched WritableStore's methods from arrow functions to regular ones

--- components/WritableStore.ts
+++ components/WritableStore.ts
@@ -13,36 +13,38 @@ export default class WritableStore<T> {
   /**
    * @returns The current value
    */
-  public get = (): T => this.value;
+  public get(): T {
+    return this.value;
+  }
 
   /**
    * Sets a new value
    * @param newValue
    */
-  public set = (newValue: T): void => {
+  public set(newValue: T): void {
     this.value = newValue;
     this.subscribeCallbacks.forEach((callback) => callback(newValue));
-  };
+  }
 
   /**
    * Exposes the current value and accepts a new value as an update.
    * @param callback
    */
-  public update = (callback: (value: T) => T): void => {
+  public update(callback: (value: T) => T): void {
     this.set(callback(this.value));
-  };
+  }
 
   /**
    * Subscribes the callback to run every time the store value changes.
    * @param callback It receives the current value.
    * @returns An unsubscribe method.
    */
-  public subscribe = (callback: (value: T) => void): (() => void) => {
+  public subscribe(callback: (value: T) => void): () => void {
     this.subscribeCallbacks.push(callback);
     callback(this.value);
 
     return () => {
       this.subscribeCallbacks = this.subscribeCallbacks.filter((subscribeCallback) => callback !== subscribeCallback);
     };
-  };
+  }
 }
