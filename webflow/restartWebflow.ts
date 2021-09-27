a8f242b9f50aed6c1399980ef921406ddb57c1af
Alex Iglesias <alexiglesias@finsweet.com>
2021-09-27 12:10:13 +0200

    restartWebflow: Added an awaitable promise as the return value

--- webflow/restartWebflow.ts
+++ webflow/restartWebflow.ts
@@ -1,11 +1,14 @@
 /**
- * Restart Webflow JS library
+ * Restarts the Webflow JS library.
+ * @returns An awaitable promise that is fulfilled when the library has been correctly reinitialized.
  */
-export const restartWebflow = (): void => {
+export const restartWebflow = async (): Promise<unknown> => {
   const { Webflow } = window;
   if (!Webflow || !('destroy' in Webflow) || !('ready' in Webflow) || !('require' in Webflow)) return;
 
   Webflow.destroy();
   Webflow.ready();
   Webflow.require('ix2')?.init();
+
+  return new Promise((resolve) => Webflow.push(() => resolve(undefined)));
 };
