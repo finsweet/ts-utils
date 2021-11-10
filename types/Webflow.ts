0c4ef392e958e55ba16e1bc8c454f42599b16d42
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-10 16:29:24 +0100

    `restartWebflow`: Added support for `e-commerce` sites.

--- types/Webflow.ts
+++ types/Webflow.ts
@@ -9,8 +9,15 @@ type Callback = () => unknown;
 export interface Webflow extends Pick<Callback[], 'push'> {
   destroy: () => void;
   ready: () => void;
-  require: (key: 'ix2') => { destroy: () => void; init: () => void } | undefined;
   env: () => boolean;
+  require: <Key extends 'ix2' | 'commerce'>(
+    key: Key
+  ) =>
+    | {
+        destroy: () => void;
+        init: Key extends 'commerce' ? (params: { siteId: string; apiUrl: string }) => void : () => void;
+      }
+    | undefined;
 }
 
 /**
--- webflow/restartWebflow.ts
+++ webflow/restartWebflow.ts
@@ -1,3 +1,5 @@
+import { getSiteId } from '.';
+
 /**
  * Restarts the Webflow JS library.
  * @returns An awaitable promise that is fulfilled when the library has been correctly reinitialized.
@@ -6,9 +8,26 @@ export const restartWebflow = async (): Promise<unknown> => {
   const { Webflow } = window;
   if (!Webflow || !('destroy' in Webflow) || !('ready' in Webflow) || !('require' in Webflow)) return;
 
+  // Global
   Webflow.destroy();
   Webflow.ready();
-  Webflow.require('ix2')?.init();
+
+  // IX2
+  const ix2 = Webflow.require('ix2');
+
+  if (ix2) {
+    ix2.destroy();
+    ix2.init();
+  }
+
+  // Commerce
+  const commerce = Webflow.require('commerce');
+  const siteId = getSiteId();
+
+  if (commerce && siteId) {
+    commerce.destroy();
+    commerce.init({ siteId, apiUrl: 'https://render.webflow.com' });
+  }
 
   return new Promise((resolve) => Webflow.push(() => resolve(undefined)));
 };
