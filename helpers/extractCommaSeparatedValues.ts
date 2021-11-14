b472ac98634e34f96649377180f9c5d3dac532e6
Alex Iglesias <alexiglesias@finsweet.com>
2021-11-14 12:05:43 +0100

    `extractCommaSeparatedValues`: Added `filterEmpty` optional param and refactored overload definition.

--- helpers/extractCommaSeparatedValues.ts
+++ helpers/extractCommaSeparatedValues.ts
@@ -1,35 +1,44 @@
 import { isKeyOf } from '../type-guards/isKeyOf';
 
 /**
- * Convert a string of comma separated values to an array of values
- * @param string Comma separated string
- * @param compareSource Acts as a type guard for making sure the extracted values match the compared source
- * @param defaultValue Is set when there is no matching results after comparing with the source
+ * Convert a string of comma separated values to an array of values.
+ *
+ * @param string Comma separated string.
+ * @param compareSource Acts as a type guard for making sure the extracted values match the compared source.
+ * @param defaultValue Is set when there is no matching results after comparing with the source.
+ * @param filterEmpty Defines if empty values should be filtered out of the returned array. Defaults to `true`.
  */
-export function extractCommaSeparatedValues(string: string | null | undefined): string[];
-export function extractCommaSeparatedValues<T extends string>(
+export function extractCommaSeparatedValues(
   string: string | null | undefined,
-  compareSource: readonly T[],
-  defaultValue?: T
-): T[];
-export function extractCommaSeparatedValues<T extends string>(
+  compareSource?: undefined,
+  defaultValue?: undefined,
+  filterEmpty?: boolean
+): string[];
+export function extractCommaSeparatedValues<SourceKey extends string, DefaultKey extends SourceKey>(
   string: string | null | undefined,
-  compareSource?: readonly T[],
-  defaultValue?: T
-): string[] | T[] {
+  compareSource: readonly SourceKey[],
+  defaultValue?: DefaultKey | undefined,
+  filterEmpty?: boolean
+): SourceKey[];
+export function extractCommaSeparatedValues<SourceKey extends string, DefaultKey extends SourceKey>(
+  string: string | null | undefined,
+  compareSource?: readonly SourceKey[] | undefined,
+  defaultValue?: DefaultKey | undefined,
+  filterEmpty = true
+): string[] | SourceKey[] {
   const emptyValue = defaultValue ? [defaultValue] : [];
   if (!string) return emptyValue;
 
   const items = string.split(',').reduce<string[]>((accumulatedValue, currentValue) => {
     const value = currentValue.trim();
 
-    if (value) accumulatedValue.push(value);
+    if (!filterEmpty || value) accumulatedValue.push(value);
 
     return accumulatedValue;
   }, []);
 
   if (compareSource) {
-    const matches = items.filter((item) => isKeyOf(item, compareSource)) as T[];
+    const matches = items.filter((item) => isKeyOf(item, compareSource)) as SourceKey[];
     return matches.length ? matches : emptyValue;
   }
 
