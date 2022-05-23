0a54d6ead1b5eb29515267b67cdbd1786d6ee128
Erik Mejia <erik@erikmejia.com>
2022-05-23 11:30:11 -0400

    feat: cleaned GH exported members

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,16 +6,4 @@ export type { PartialExcept } from './PartialExcept';
 export type { PickPartial } from './PickPartial';
 export type { PickRequired } from './PickRequired';
 export type { RequiredExcept } from './RequiredExcept';
-export {
-  DepartmentsResponse,
-  JobsResponse,
-  JobUniqueResponse,
-  OfficesResponse,
-  Job,
-  JobWithContent,
-  JobWithQuestions,
-  Question,
-  Field,
-  Department,
-  Office,
-} from './apis/Greenhouse';
+export * from './apis/Greenhouse';
