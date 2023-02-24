73ec4dd4a1a40a121a323cfa851e9053646398ac
jaji oluwatobiloba <jajmantobi@gmail.com>
2023-02-24 12:06:14 -0500

    Implement vitepress template

---
+++ docs/.vitepress/cache/deps/@finsweet_docs-theme.js
@@ -0,0 +1,8 @@
+// node_modules/@finsweet/docs-theme/src/index.ts
+import "/Users/macc/Desktop/finsweet/ts-utils/node_modules/@finsweet/docs-theme/src/custom.css";
+import DefaultTheme from "vitepress/theme";
+var src_default = DefaultTheme;
+export {
+  src_default as default
+};
+//# sourceMappingURL=@finsweet_docs-theme.js.map
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,3 @@
+import Theme from '@finsweet/docs-theme';
+
+export default Theme;
